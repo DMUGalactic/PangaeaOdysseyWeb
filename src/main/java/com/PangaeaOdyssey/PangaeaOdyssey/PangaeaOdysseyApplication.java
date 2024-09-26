309c3df8dc4ed311d803edd1ffbadee6ed643731
seoseunghyun1025 <seunghyun.seo101@gmail.com>
2024-09-26 20:42:47 +0900

    커밋 테스트

--- src/main/java/com/PangaeaOdyssey/PangaeaOdyssey/PangaeaOdysseyApplication.java
+++ src/main/java/com/PangaeaOdyssey/PangaeaOdyssey/PangaeaOdysseyApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PangaeaOdysseyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PangaeaOdysseyApplication.class, args);
-		System.out.println("hello world!");
-	}
+	public static void main(String[] args) {SpringApplication.run(PangaeaOdysseyApplication.class, args);}
 
 }
