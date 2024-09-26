a34ab1896a95a0046b956e271f5493a968ee8774
SeoSeungHyun <seunghyun.seo101@gmail.com>
2024-09-26 20:24:06 +0900

    first commit

---
+++ src/main/java/com/PangaeaOdyssey/PangaeaOdyssey/PangaeaOdysseyApplication.java
@@ -0,0 +1,13 @@
+package com.PangaeaOdyssey.PangaeaOdyssey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangaeaOdysseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangaeaOdysseyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PangaeaOdyssey/PangaeaOdyssey/PangaeaOdysseyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PangaeaOdyssey.PangaeaOdyssey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PangaeaOdysseyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
