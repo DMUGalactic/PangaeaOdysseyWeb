10993d03f27f48f656dbba10cd66b2dc23d6f12e
seoseunghyun1025 <seunghyun.seo101@gmail.com>
2024-09-26 20:48:59 +0900

    feacher commit test

--- src/main/java/com/PangaeaOdyssey/PangaeaOdyssey/PangaeaOdysseyApplication.java
+++ src/main/java/com/PangaeaOdyssey/PangaeaOdyssey/PangaeaOdysseyApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PangaeaOdysseyApplication {
 
-	public static void main(String[] args) {SpringApplication.run(PangaeaOdysseyApplication.class, args);}
+	public static void main(String[] args) {SpringApplication.run(PangaeaOdysseyApplication.class, args);
+		System.out.println("hello world!");
+	}
 
 }
