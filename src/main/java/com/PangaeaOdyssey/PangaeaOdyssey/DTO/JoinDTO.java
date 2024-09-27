3abd61bb8c4e747bd5a3c36736241700f3cce815
seoseunghyun1025 <seunghyun.seo101@gmail.com>
2024-09-27 16:22:23 +0900

    join

---
+++ src/main/java/com/PangaeaOdyssey/PangaeaOdyssey/DTO/JoinDTO.java
@@ -0,0 +1,14 @@
+package com.PangaeaOdyssey.PangaeaOdyssey.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JoinDTO {
+    private String id;
+    private String password;
+    private String nickname;
+    private String email;
+    private String filename;
+}
---
+++ src/main/java/com/PangaeaOdyssey/PangaeaOdyssey/Service/JoinService.java
@@ -0,0 +1,41 @@
+package com.PangaeaOdyssey.PangaeaOdyssey.Service;
+
+import com.PangaeaOdyssey.PangaeaOdyssey.Authority;
+import com.PangaeaOdyssey.PangaeaOdyssey.DTO.JoinDTO;
+import com.PangaeaOdyssey.PangaeaOdyssey.Entity.Member;
+import com.PangaeaOdyssey.PangaeaOdyssey.Repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void joinProcess(JoinDTO joinDTO) {
+
+        String id = joinDTO.getId();
+        String password = joinDTO.getPassword();
+        String email = joinDTO.getEmail();
+        String filename = joinDTO.getFilename();
+
+        Boolean isExist = memberRepository.existsById(id);
+
+        if (isExist) {
+            return;
+        }
+
+        Member data = new Member();
+
+        data.setId(id);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setEmail(email);
+        data.setFilename(filename);
+        data.setRole(Authority.USER);
+
+        memberRepository.save(data);
+    }
+}
