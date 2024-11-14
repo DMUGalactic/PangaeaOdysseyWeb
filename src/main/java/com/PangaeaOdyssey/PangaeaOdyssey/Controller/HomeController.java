69557c72832ebab6d6ec69b7afd23d82151345a0
seoseunghyun1025 <seunghyun.seo101@gmail.com>
2024-11-14 21:00:10 +0900

    브랜치 변경 왜 안돼

--- src/main/java/com/PangaeaOdyssey/PangaeaOdyssey/Controller/HomeController.java
+++ src/main/java/com/PangaeaOdyssey/PangaeaOdyssey/Controller/HomeController.java
@@ -12,6 +12,7 @@ public class HomeController {
     @GetMapping("/home")
     public String homeP() {
         System.out.println("helloworld");
+        System.out.println("컨피그 테스트");
         return "home.html";  // home.html 페이지로 이동 (src/main/resources/templates/home.html)
     }
 }
